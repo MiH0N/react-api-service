7138b8919dea978a8053ff1d3fb0b17c96c19630
@mehrab.hosseini14 <mehrab.hosseini14@gmail.com>
2024-05-08 04:08:49 +0330

    feature : use http request hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,23 @@ import useFetch from './hooks/useFetch';
 import './App.css'
 
 const fetchData = async (signal: AbortSignal) => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/todos', { signal });
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1', { signal });
   const data = await response.json();
   return { data };
 };
 
+type TodoProps = {
+  id: number;
+  userId: number;
+  title: string;
+  completed: boolean
+}
+
 function App() {
-  const { isLoading, data, refetch } = useFetch({
+  const { isLoading, data, refetch } = useFetch<TodoProps>({
     apiHandler: fetchData,
-    initialDataType: { data: null }, // Example initial data type
+    key: 'todo',
+    cacheTime: 10000
   });
 
   return (
@@ -18,9 +26,15 @@ function App() {
       <button onClick={() => refetch()}>
         refetch
       </button>
-      <p>
-        {isLoading ? 'isLoading .......' : 'done!'}
-      </p>
+      {isLoading ?
+        <p>'isLoading .......'</p> :
+        (
+          <div className='todo'>
+            <p>task : {data?.title}</p>
+            {data?.completed ? <span>✅</span> : <span>❌</span>}
+          </div>
+        )}
+
     </div>
   )
 }

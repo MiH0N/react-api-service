013d3ecc0b3e0d50e307b04fb9d7c1970315b931
@mehrab.hosseini14 <mehrab.hosseini14@gmail.com>
2024-05-08 03:32:28 +0330

    feature: use useFetch hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import useFetch from './hooks/useFetch';
 import './App.css'
 
+const fetchData = async (signal: AbortSignal) => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos', { signal });
+  const data = await response.json();
+  return { data };
+};
+
 function App() {
-  const [count, setCount] = useState(0)
+  const { isLoading, data, refetch } = useFetch({
+    apiHandler: fetchData,
+    initialDataType: { data: null }, // Example initial data type
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="card">
+      <button onClick={() => refetch()}>
+        refetch
+      </button>
+      <p>
+        {isLoading ? 'isLoading .......' : 'done!'}
       </p>
-    </>
+    </div>
   )
 }
 
